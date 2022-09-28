29b1314268437bfa04f381f9c2a84dfb0813bf4b
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-09-28 18:07:50 -0300

    Update src/screens/HomeScreen/patterns/GuidesGrid/index.tsx

--- src/screens/HomeScreen/patterns/GuidesGrid/index.tsx
+++ src/screens/HomeScreen/patterns/GuidesGrid/index.tsx
@@ -4,7 +4,6 @@ import { HomeGetAllGuidesQuery } from "@src/gql_types";
 import styled from "styled-components";
 
 const StyledBox = styled.li<any>`
-  ///
   display: flex;
   flex: 0 0 calc(100% - 16px);
   align-items: center;
