cb6eb8ae8a0bd4de5eaef9edcc084744d361e85c
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-10-02 20:44:32 -0300

    update

--- _scripts/modules/createShareableGuides/index.ts
+++ _scripts/modules/createShareableGuides/index.ts
@@ -1,9 +1,52 @@
+import path from "path";
+import fs from "fs";
+import { HomeGetAllGuidesDocument, SiteLocale } from "@src/gql_types";
+import { initializeApollo } from "@src/infra/apolloClient";
 import { log } from "../../infra/log";
 
 log("module/createShareableGuides");
+main();
 
-export function createShareableGuides() {
-  log("Running...");
+export async function main() {
+  const apolloClient = initializeApollo();
+  const locale = SiteLocale.PtBr;
+
+  const { data } = await apolloClient.query({
+    query: HomeGetAllGuidesDocument,
+    variables: {
+      locale,
+    },
+  });
+
+  data.guides.forEach(({ slug, name }) => {
+    const filePath = path.resolve(
+      ".",
+      "_data",
+      "downloadFiles",
+      locale,
+      `${slug}`
+    );
+
+    log(`Creating shareable guide: ${filePath}`);
+
+    fs.writeFileSync(
+      `${filePath}.md`,
+      templateMarkdown({
+        name,
+      })
+    );
+  });
 }
 
-createShareableGuides();
+// ==============================
+// ==============================
+
+interface MarkdownTemplate {
+  name: string;
+}
+function templateMarkdown({ name }: MarkdownTemplate) {
+  const template = `
+# ${name}
+`;
+  return template.split("\n").slice(1).join("\n");
+}
