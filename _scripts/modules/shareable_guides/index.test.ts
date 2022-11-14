4980e0b70c625f540a5e5cb131731730c16191fa
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-11-14 08:07:12 -0800

    feat: add initial implementation of download guide

--- _scripts/modules/shareable_guides/index.test.ts
+++
@@ -1,8 +0,0 @@
-import * as shareableGuide from "./index";
-
-describe("sample test", () => {
-  it("should pass", () => {
-    shareableGuide.main();
-    expect(true).toBe(true);
-  });
-});
--- _scripts/modules/shareable_guides/index.ts
+++ _scripts/modules/shareable_guides/index.ts
@@ -1,6 +1,9 @@
-// import { initializeApollo } from "@src/infra/apolloClient";
-import { log } from "../infra/log";
+import { initializeApollo } from "@src/infra/apolloClient";
+import { log } from "@scripts/modules/infra/log";
 
 export const main = () => {
   log("Is this working?");
+  initializeApollo();
 };
+
+main();
