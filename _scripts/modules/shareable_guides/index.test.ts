cda244af43a50be91c9e5c1db4721224b7f63264
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-11-14 07:39:46 -0800

    update

---
+++ _scripts/modules/shareable_guides/index.test.ts
@@ -0,0 +1,8 @@
+import * as shareableGuide from "./index";
+
+describe("sample test", () => {
+  it("should pass", () => {
+    shareableGuide.main();
+    expect(true).toBe(true);
+  });
+});
---
+++ _scripts/modules/shareable_guides/index.ts
@@ -0,0 +1,6 @@
+// import { initializeApollo } from "@src/infra/apolloClient";
+import { log } from "../infra/log";
+
+export const main = () => {
+  log("Is this working?");
+};
