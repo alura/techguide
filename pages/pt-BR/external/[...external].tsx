4011d4f47600c8752b0427a67067f76c55091e90
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-07 15:01:20 -0300

    update

--- pages/pt-BR/external/[...external].tsx
+++ pages/pt-BR/external/[...external].tsx
@@ -27,117 +27,109 @@ function slugify(text: string) {
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const locale = (ctx.locale || SiteLocale.PtBr) as SiteLocale;
   const data = {};
-  const [username, repo, branch, path] = ctx.params.external as string[];
-  const URL = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/${path.replace(
-    "__",
-    "."
-  )}`;
+  // const [username, repo, branch, path] = ctx.params.external as string[];
+  // const URL = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/${path.replace(
+  //   "__",
+  //   "."
+  // )}`;
+  // eslint-disable-next-line no-console
+  // console.log(URL);
+  // if (URL.includes("undefined")) {
+  //   return {
+  //     notFound: true,
+  //   };
+  // }
+
+  // const response = await fetch(URL).then((res) => res.json());
+  const response = {
+    name: "Nome do Guia",
+    expertises: [
+      {
+        name: "Nivel 1 - Básico",
+        cards: [
+          {
+            name: "Nome do Cartão Customizado",
+            description: "Descrição do Cartão",
+            link: "Link do Cartão",
+            "key-objectives": ["Objetivo 1", "Objetivo 2", "Objetivo 3"],
+            contents: [
+              {
+                type: "SITE",
+                title: "Node.js - Documentation",
+                link: "https://nodejs.dev/en/learn/",
+              },
+            ],
+          },
+          {
+            id: "nodejs-fundamentals",
+          },
+        ],
+      },
+      {
+        name: "Nivel 2 - Intermediário",
+        cards: [],
+      },
+      {
+        name: "Nivel 3 - Avançado",
+        cards: [],
+      },
+    ],
+    collaborations: [
+      {
+        name: "Lado Esquerdo do T",
+        cards: [],
+      },
+      {
+        name: "Lado Direito do T",
+        cards: [],
+      },
+    ],
+  };
 
   // eslint-disable-next-line no-console
-  console.log(URL);
+  console.log(response);
 
-  if (URL.includes("undefined")) {
-    return {
-      notFound: true,
-    };
-  }
-
-  try {
-    // const response = await fetch(URL).then((res) => res.json());
-    const response = {
-      name: "Nome do Guia",
-      expertises: [
-        {
-          name: "Nivel 1 - Básico",
-          cards: [
-            {
-              name: "Nome do Cartão Customizado",
-              description: "Descrição do Cartão",
-              link: "Link do Cartão",
-              "key-objectives": ["Objetivo 1", "Objetivo 2", "Objetivo 3"],
-              contents: [
-                {
-                  type: "SITE",
-                  title: "Node.js - Documentation",
-                  link: "https://nodejs.dev/en/learn/",
-                },
-              ],
-            },
-            {
-              id: "nodejs-fundamentals",
-            },
-          ],
-        },
-        {
-          name: "Nivel 2 - Intermediário",
-          cards: [],
-        },
-        {
-          name: "Nivel 3 - Avançado",
-          cards: [],
-        },
-      ],
-      collaborations: [
-        {
-          name: "Lado Esquerdo do T",
-          cards: [],
-        },
-        {
-          name: "Lado Direito do T",
-          cards: [],
-        },
-      ],
-    };
-
-    // eslint-disable-next-line no-console
-    console.log(response);
-
-    const guide = Object.entries(response).reduce((acc, [key, value]) => {
-      if (Array.isArray(value)) {
-        return {
-          ...acc,
-          [key]: value.map((item) => {
-            return {
-              ...item,
-              blocks: item.cards
-                .map((card) => {
-                  if (card.id) return null;
-                  return {
-                    item: {
-                      slug: slugify(card.name),
-                      id: card.name,
-                      ...card,
-                    },
-                  };
-                })
-                .filter(Boolean),
-            };
-          }),
-        };
-      }
-
+  const guide = Object.entries(response).reduce((acc, [key, value]) => {
+    if (Array.isArray(value)) {
       return {
         ...acc,
-        [key]: value,
+        [key]: value.map((item) => {
+          return {
+            ...item,
+            blocks: item.cards
+              .map((card) => {
+                if (card.id) return null;
+                return {
+                  item: {
+                    slug: slugify(card.name),
+                    id: card.name,
+                    ...card,
+                  },
+                };
+              })
+              .filter(Boolean),
+          };
+        }),
       };
-    }, {});
+    }
 
-    return withLocaleContent(
-      {
-        props: {
-          ...data,
-          guide,
-          locale,
-          revalidate: 60,
-        },
-      },
-      locale
-    );
-  } catch (error) {
     return {
-      notFound: true,
+      ...acc,
+      [key]: value,
     };
-  }
+  }, {});
+
+  return withLocaleContent(
+    {
+      props: {
+        ...data,
+        guide,
+        locale,
+        revalidate: 60,
+      },
+    },
+    locale
+  );
 };
 
 export { default } from "@src/screens/PathScreen";
