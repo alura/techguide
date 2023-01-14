2a710f6f37b6c80d852cefc2d1d5af3ac1888464
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-13 22:47:37 -0300

    feat: add feature code

---
+++ pages/pt-BR/external/[...external].ts
@@ -0,0 +1,91 @@
+import { SiteLocale } from "@src/gql_types";
+import { withLocaleContent } from "@src/infra/i18n/withLocaleContent";
+import { GetStaticProps } from "next";
+
+export async function getStaticPaths(ctx) {
+  // eslint-disable-next-line no-console
+  console.log(ctx);
+
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+function slugify(text: string) {
+  return text
+    .toString()
+    .normalize("NFD") // split an accented letter in the base letter and the accent
+    .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 -]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+    .replace(/\s+/g, "-") // separator
+    .replace(/-+/g, "-"); // remove repeated separator
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const locale = (ctx.locale || SiteLocale.PtBr) as SiteLocale;
+  const data = {};
+  const [username, repo, branch, path] = ctx.params.external as string[];
+  const URL = `https://raw.githubusercontent.com/${username}/${repo}/${branch}/${path.replace(
+    "__",
+    "."
+  )}`;
+  // eslint-disable-next-line no-console
+  console.log(URL);
+  if (URL.includes("undefined")) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const response = await fetch(URL).then((res) => res.json());
+
+  // eslint-disable-next-line no-console
+  console.log(response);
+
+  const guide = Object.entries(response).reduce((acc, [key, value]) => {
+    if (Array.isArray(value)) {
+      return {
+        ...acc,
+        [key]: value.map((item) => {
+          return {
+            ...item,
+            cards: item.cards
+              .map((card) => {
+                if (card.id) return null;
+                return {
+                  item: {
+                    slug: slugify(card.name),
+                    id: card.name,
+                    ...card,
+                  },
+                };
+              })
+              .filter(Boolean),
+          };
+        }),
+      };
+    }
+
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+
+  return withLocaleContent(
+    {
+      props: {
+        ...data,
+        guide,
+        locale,
+      },
+      revalidate: 60,
+    },
+    locale
+  );
+};
+
+export { default } from "@src/screens/PathScreen";
---
+++ pages/pt-BR/my.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { SiteLocale } from "@src/gql_types";
+
+export default function ToScreen({ locale }: { locale: SiteLocale }) {
+  const router = useRouter();
+  // http://localhost:3000/pt-BR/my?url=https://github.com/omariosouto/omariosouto/blob/main/techguide-v1.json
+  // ...
+  React.useEffect(() => {
+    if (router.isReady) {
+      const [localePrefix, localeSufix] = locale.split("_");
+      const urlLocale = `${localePrefix.toLowerCase()}${
+        localeSufix ? `-${localeSufix}` : localeSufix
+      }`;
+
+      const url = router.query.url as string;
+      const [, , , username, repo, , branch, path] = url.split("/");
+
+      const nextUrl = `/${urlLocale}/external/${username}/${repo}/${branch}/${path.replaceAll(
+        ".",
+        "__"
+      )}/`;
+      router.push(nextUrl);
+    }
+  }, [router]);
+
+  return <div>Redirect...</div>;
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      locale: SiteLocale.PtBr,
+    },
+  };
+};
