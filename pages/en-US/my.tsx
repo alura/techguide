a8fdf5e8a5f10b1be5efdaf466c349fc9972c4ea
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2023-01-22 15:29:55 -0300

    feat: generate your guide

--- pages/en-US/my.tsx
+++ pages/en-US/my.tsx
@@ -1,11 +1,10 @@
 import { SiteLocale } from "@src/gql_types";
+import { getStaticProps as getStaticPropsPtBr } from "../pt-BR/my";
 
 export { default } from "../pt-BR/my";
 
 export const getStaticProps = async () => {
-  return {
-    props: {
-      locale: SiteLocale.EnUs,
-    },
-  };
+  return getStaticPropsPtBr({
+    locale: SiteLocale.EnUs,
+  });
 };
