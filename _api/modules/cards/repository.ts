f1a6d5751febdb3cc84fdb7cc32ce18e24294bfc
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-09-20 11:38:52 -0300

    update

--- _api/modules/cards/repository.ts
+++ _api/modules/cards/repository.ts
@@ -29,7 +29,7 @@ export function cardsRepository() {
       const cardsPromise = cardFileNames.map(async (fileName) => {
         const slug = slugify(fileName.split(".")[0]);
         // eslint-disable-next-line no-console
-        // console.log("--", fileName);
+        console.log("--", fileName);
 
         const cardCache = await storage.get(`card-${locale}-${slug}`);
         if (cardCache) return cardCache;
